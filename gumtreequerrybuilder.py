db9aed4fa8d410de1f997178a451e082df544c18
mateusz <kontakt@mateuszmidor.com>
2014-07-24 00:10:07 +0200

    Refactored gumtreequerrybuilder.py

--- gumtreequerrybuilder.py
+++ gumtreequerrybuilder.py
@@ -1,4 +1,4 @@
-TEMPLATE = 'http://www.gumtree.pl/fp-mieszkania-i-domy-do-wynajecia/{_city}/{_whereabouts}c9008l3200208?A_AreaInMeters_max={_maxArea}&A_AreaInMeters_min={_minArea}&A_ForRentBy=ownr&A_NumberRooms={_numRooms}&AdType=2&isSearchForm=true&maxPrice={_maxPrice}&maxPriceBackend=200000&minPrice={_minPrice}&minPriceBackend=100000&Page='
+TEMPLATE = 'http://www.gumtree.pl/fp-mieszkania-i-domy-do-wynajecia/{_city}/{_whereabouts}c9008l3200208?A_AreaInMeters_max={_maxArea}&A_AreaInMeters_min={_minArea}&A_ForRentBy=ownr&A_NumberRooms={_numRooms}&AdType=2&isSearchForm=true&maxPrice={_maxPrice}&maxPriceBackend=200000&minPrice={_minPrice}&minPriceBackend=100000'
 
 def build(city = "", whereabouts = "", numRooms = "", minPrice = "", maxPrice = "", minArea = "", maxArea = ""):
     if (whereabouts != ""):
@@ -8,5 +8,4 @@ def build(city = "", whereabouts = "", numRooms = "", minPrice = "", maxPrice = 
     
     querry = TEMPLATE.format(_city = city, _whereabouts = whereabouts, _numRooms = numRooms, _minPrice = minPrice, _maxPrice = maxPrice,
                            _minArea = minArea, _maxArea = maxArea)
-    print querry
     return querry
