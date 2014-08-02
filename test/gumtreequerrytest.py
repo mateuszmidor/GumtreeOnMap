65b0bdf350480f3701df1b1428401e8f1779a24d
mateusz <kontakt@mateuszmidor.com>
2014-08-02 22:11:21 +0200

    Added testAllParamsSupplied to gumtreequerrytest.py

--- test/gumtreequerrytest.py
+++ test/gumtreequerrytest.py
@@ -19,6 +19,17 @@ class Test(unittest.TestCase):
         except:
             self.fail("Unexpected exception type thrown")
 
+    def testAllParamsSupplied(self):
+        EXPECTED_QUERRY = 'http://www.gumtree.pl/fp-mieszkania-i-domy-do-wynajecia/Krakow/Nowa+Huta/c9008l3200208?A_AreaInMeters_max=70&A_AreaInMeters_min=30&A_ForRentBy=ownr&A_NumberRooms=10&AdType=2&isSearchForm=true&maxPrice=1000&maxPriceBackend=200000&minPrice=500&minPriceBackend=100000'
+        querry = GumtreeQuerry.compose(city="Krakow", 
+                                       whereabouts="Nowa Huta", 
+                                       numRooms="1", 
+                                       minPrice="500", 
+                                       maxPrice="1000",
+                                       minArea="30",
+                                       maxArea="70")
+        self.assertEquals(EXPECTED_QUERRY, querry)
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testRaisesOnEmptyCity']
     unittest.main()
