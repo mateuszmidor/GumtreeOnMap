0aabca2187c9e433f1f4b479e088ee75dbf5208c
mateusz <kontakt@mateuszmidor.com>
2014-08-03 20:03:23 +0200

    Corrected template web page name in DesktopView, added OnlineView.htm with input boxes for filtering

--- src/desktopview.py
+++ src/desktopview.py
@@ -12,7 +12,7 @@ class DesktopView():
     
     @staticmethod
     def getOfferPage(mapPoints, mapCenter, mapZoom):
-        offerPage = WebPageTemplate.fromFile("../data/DesktopView.html")
+        offerPage = WebPageTemplate.fromFile("../data/DesktopView.htm")
         offerPage.setField(u"$POINTS$", mapPoints.asJavaScript())
         offerPage.setField(u"$MAP_CENTER_LONG$", mapCenter[0])
         offerPage.setField(u"$MAP_CENTER_LATT$", mapCenter[1])
