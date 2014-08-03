68bd95b31af928aa026c3c7cd7ebca50fbccfbf3
mateusz <kontakt@mateuszmidor.com>
2014-08-03 09:11:44 +0200

    AddressResolver now returns full address, not only street/district name, eg 'wielicka 2, krakow, polska'

--- src/addressresolver.py
+++ src/addressresolver.py
@@ -13,8 +13,12 @@ class AddressResolver():
     '''
     classdocs
     '''
+    
+    # this country will be added as suffix to every returned address
+    OPERATING_COUNTRY = u"polska"
+    
     @staticmethod
-    def resolve(defaulAddress, *sources):
+    def resolve(cityToLookForTheAddressIn, *sources):
         # this is the first place to speedup; longers from 4 -> 10 sec/25offer
         # introduce offer cache?
         address = streetExtractor.extract(*sources)
@@ -22,10 +26,15 @@ class AddressResolver():
         if (not address):
             address = districtExtractor.extract(*sources)
             
+        return AddressResolver.formFullAddress(address, cityToLookForTheAddressIn)
+
+    @staticmethod
+    def formFullAddress(address, cityToLookForTheAddressIn):
         # TODO:  City extractor
         if (not address):
             #address = cityExtractor.extract(addressSection, title, summary)
-            address = defaulAddress
-            
-        return address.strip()
-        
+            return "%s, %s" % (cityToLookForTheAddressIn, AddressResolver.OPERATING_COUNTRY)
+        else:
+            return "%s, %s, %s" % (address, cityToLookForTheAddressIn, AddressResolver.OPERATING_COUNTRY)
+    
+    
--- test/addressresolvertest.py
+++ test/addressresolvertest.py
@@ -13,20 +13,20 @@ class Test(unittest.TestCase):
     def testResolveStreetWithNumber(self):
         address = u"Wielicka 2, Krakow"
         source = u"Do wynajecia przytulne mieszkanie pod adresem %s \n 3 pokoje w cenie dwoch" % address
-        resolved = AddressResolver.resolve(u'defaultAddress', source)
-        self.assertEquals(u"wielicka 2", resolved)
+        resolved = AddressResolver.resolve(u'krakow', source)
+        self.assertEquals(u"wielicka 2, krakow, polska", resolved)
         
     def testResolveDistrict(self):
         address = u"Ruczaj"
         source = u"Do wynajecia przytulne mieszkanie w dzielnicy %s \n 3 pokoje w cenie dwoch" % address
-        resolved = AddressResolver.resolve(u'defaultAddress', source)
-        self.assertEquals(u"ruczaj", resolved)
+        resolved = AddressResolver.resolve(u'krakow', source)
+        self.assertEquals(u"ruczaj, krakow, polska", resolved)
 
-    def testResolveFallbackToDefaultAddres(self):
-        address = u"Łodygowice"
+    def testResolveFailAndFallbackToCityOnly(self):
+        address = u"Łodygowice" # theres no łodygowice in krakow :)
         source = u"Do wynajecia przytulne mieszkanie w dzielnicy %s \n 3 pokoje w cenie dwoch" % address
-        resolved = AddressResolver.resolve(u'defaultAddress', source)
-        self.assertEquals(u"defaultAddress", resolved)        
+        resolved = AddressResolver.resolve(u'krakow', source)
+        self.assertEquals(u'krakow, polska', resolved)        
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testResolveStreetWithNumber']
