06a00b2c4e5774b56ec36cb5bc0072895e0b3e09
mateusz <kontakt@mateuszmidor.com>
2014-08-24 18:54:19 +0200

    Reduced MAX_THREADS from 7 to 6 to ensure better stability (cant start new thread error)

--- src/gumtreeofferswithcache.py
+++ src/gumtreeofferswithcache.py
@@ -20,7 +20,7 @@ class GumtreeOffersWithCache():
     @staticmethod
     def askForOffers(querry, count):
         # value limited by netmark host
-        MAX_THREADS = 7 
+        MAX_THREADS = 6 
         querryUrl = str(querry)
         urls = GumtreeOffersWithCache.offerurls.getUrls(querryUrl, count)
         cityAddressResolver = GumtreeOffersWithCache.addressresolver.forCity(querry.city)
